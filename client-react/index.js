19ded1e3685b2a5fad89fe820973014c0266cab6
aviralrabbit1 <aviral.robin@gmail.com>
2023-07-05 02:13:35 +0530

    Modifications for deployment

---
+++ client-react/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const axios = require("axios");
+const PORT = process.env.PORT || 3001;
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+const buildPath = path.join(__dirname, 'build')
+app.use(express.static(buildPath))
+
+// app.use(express.static(path.join(__dirname, "/public"))); 
+
+const CHAT_ENGINE_PROJECT_ID = "5b52eb88-3e1f-4e35-97a5-a47d2c86ee97";
+const CHAT_ENGINE_PRIVATE_KEY = "f7ba934a-ddbe-416f-b87a-3d5f88017abf";
+
+app.post("/signup", async (req, res) => {
+  const { username, secret, email, first_name, last_name } = req.body;
+
+  // Store a user-copy on Chat Engine!
+  // Docs at rest.chatengine.io
+  try {
+    const r = await axios.post(
+      "https://api.chatengine.io/users/",
+      { username, secret, email, first_name, last_name },
+      { headers: { "Private-Key": CHAT_ENGINE_PRIVATE_KEY } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, secret } = req.body;
+
+  // Fetch this user from Chat Engine in this project!
+  // Docs at rest.chatengine.io
+  try {
+    const r = await axios.get("https://api.chatengine.io/users/me/", {
+      headers: {
+        "Project-ID": CHAT_ENGINE_PROJECT_ID,
+        "User-Name": username,
+        "User-Secret": secret,
+      },
+    });
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+// gets the static files from the build folder
+app.get('*', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'))
+})
+
+// vvv On port 3001!
+
+app.listen(PORT);
